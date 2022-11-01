845b06626ba54d4fec19ed8a0ffd319e0e876ac7
Gavin D'souza <gavin18d@gmail.com>
2022-11-01 15:02:19 +0530

    test: Server Performance Log

--- server_performance/server_performance/doctype/server_performance_log/test_server_performance_log.py
+++ server_performance/server_performance/doctype/server_performance_log/test_server_performance_log.py
@@ -1,10 +1,24 @@
 # Copyright (c) 2022, Raffael Meyer and Contributors
 # See license.txt
 
+import frappe
 from server_performance.server_performance.doctype.server_performance_log.server_performance_log import ServerPerformanceLog
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestServerPerformanceLog(FrappeTestCase):
+	def test_server_performance_log(self):
+		performance_log = frappe.new_doc("Server Performance Log")
+
+		self.assertIsNone(performance_log.cpu_utilisation)
+		self.assertIsNone(performance_log.ram_utilisation)
+		self.assertIsNone(performance_log.disk_utilisation)
+
+		performance_log.save()
+
+		self.assertIsInstance(performance_log.cpu_utilisation, float)
+		self.assertIsInstance(performance_log.ram_utilisation, float)
+		self.assertIsInstance(performance_log.disk_utilisation, float)
+
 	def test_clear_old_logs(self):
 		ServerPerformanceLog.clear_old_logs(days=7)
