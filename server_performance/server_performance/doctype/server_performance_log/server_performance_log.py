b5e60e36411da1d1d47256b022acafac5f1cd867
Gavin D'souza <gavin18d@gmail.com>
2022-11-01 15:01:36 +0530

    refactor: Move server performance logging logic to controller

--- server_performance/server_performance/doctype/server_performance_log/server_performance_log.py
+++ server_performance/server_performance/doctype/server_performance_log/server_performance_log.py
@@ -5,9 +5,15 @@ import frappe
 from frappe.model.document import Document
 from frappe.query_builder import Interval
 from frappe.query_builder.functions import Now
+from psutil import cpu_percent, disk_usage, virtual_memory
 
 
 class ServerPerformanceLog(Document):
+	def before_insert(self):
+		self.cpu_utilisation = cpu_percent()
+		self.ram_utilisation = virtual_memory().percent
+		self.disk_utilisation = disk_usage(frappe.get_site_path()).percent
+
 	@staticmethod
 	def clear_old_logs(days=90):
 		table = frappe.qb.DocType("Server Performance Log")
