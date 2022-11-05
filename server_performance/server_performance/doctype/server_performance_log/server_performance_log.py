5fe67d791693bd0fa7bfda4a2441302b77341ab8
Ankush Menat <ankush@frappe.io>
2022-11-05 13:16:06 +0530

    perf: faster log deletion
    `modified` column is indexed by default on doctypes (for list views) so deleting using the modified column is much faster as data grows. 
    
    For logs: modified ~= creation. So it's correct.

--- server_performance/server_performance/doctype/server_performance_log/server_performance_log.py
+++ server_performance/server_performance/doctype/server_performance_log/server_performance_log.py
@@ -11,4 +11,4 @@ class ServerPerformanceLog(Document):
 	@staticmethod
 	def clear_old_logs(days=90):
 		table = frappe.qb.DocType("Server Performance Log")
-		frappe.db.delete(table, filters=(table.creation < (Now() - Interval(days=days))))
+		frappe.db.delete(table, filters=(table.modified < (Now() - Interval(days=days))))
